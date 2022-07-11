a7dec14535c375f24e35ae4a47603fd11a033942
Sergey Lanovoy <sla@kode.ru>
2022-07-11 11:44:39 +0200

    clear dep modules

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,59 +1,59 @@
 plugins {
-    id ("com.android.application")
-    kotlin ("android")
+  id("com.android.application")
+  kotlin("android")
 }
 
 android {
-    compileSdk = 32
+  compileSdk = 32
 
-    defaultConfig {
-        applicationId = "com.sla.baseproject"
-        minSdk = 21
-        targetSdk = 32
-        versionCode = 1
-        versionName = "1.0"
+  defaultConfig {
+    applicationId = "com.sla.baseproject"
+    minSdk = 21
+    targetSdk = 32
+    versionCode = 1
+    versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    vectorDrawables {
+      useSupportLibrary = true
     }
+  }
 
-    buildTypes {
-        release {
-            isMinifyEnabled =  false
-            proguardFiles (
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(
+        getDefaultProguardFile("proguard-android-optimize.txt"),
+        "proguard-rules.pro"
+      )
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+  kotlinOptions {
+    jvmTarget = "1.8"
+  }
+  buildFeatures {
+    compose = true
+  }
+  composeOptions {
+    kotlinCompilerExtensionVersion = "1.2.0-beta03"
+  }
+  packagingOptions {
+    resources {
+      excludes += "/META-INF/{AL2.0,LGPL2.1}"
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.2.0-beta03"
-    }
-    packagingOptions {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-    namespace = "com.sla.baseproject"
+  }
+  namespace = "com.sla.baseproject"
 }
 
 dependencies {
-    implementation (libs.androidXCore)
-    implementation (libs.androidXCoreKtx)
-    implementation (libs.activityCompose)
+  implementation(libs.androidXCore)
+  implementation(libs.androidXCoreKtx)
+  implementation(libs.activityCompose)
 
-    api(libs.bundles.androidUiLibs)
-    api(libs.bundles.lifecycleLibs)
+  api(libs.bundles.androidUiLibs)
+  api(libs.bundles.lifecycleLibs)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,11 +21,3 @@ rootProject.name = "BaseProject"
 include (
     ":app"
 )
-include(":core:ui")
-include(":core:domain")
-include(":core:data")
-include(":feature")
-include(":feature:app")
-include(":feature:app:ui")
-include(":feature:app:domain")
-include(":feature:app:data")
